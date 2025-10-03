e0933e420f1c759f5c7b232c597c6270b45cb7b1
Arishvi <arish.uniq@gmail.com>
2025-10-03 19:02:38 +0530

    jsFile

---
+++ index.js
@@ -0,0 +1,377 @@
+console.log('Hello World')
+
+//varibles
+
+// let student1="Nithish kumar"  // let - keywords  //student1 - identifier 
+
+// let student2='Jayaprakasham'
+
+// console.log(student1);
+// console.log(student2);
+
+// js rules
+
+// meaningful name
+// 1st letter alaptic character
+//its camelCase senstive
+//  dont use symbols 
+
+// let javaScript='Arish'
+
+
+// data types
+
+// primitive type
+// non primitive
+
+// primitive type
+
+// 1.string
+// 2.number
+// 3.boolean
+// 4.undefined
+// 5.null
+
+// string
+
+// let str="jayaprakasam123@#"
+
+// let str1='nithish456@#'
+
+// console.log(str)
+
+// console.log(str1)
+
+// number
+
+// let num=12345
+
+// console.log(12345)
+
+// boolean
+
+// let bool=false
+
+// console.log(bool);
+
+// undefined
+
+// let unf=undefined
+
+// console.log(unf);
+
+// null
+
+// let nu=null
+
+// console.log(nu);
+
+// let nul=undefined
+
+// console.log(nul)
+
+// non primitive type
+
+// Array
+
+// object
+
+// function
+
+
+// var let const
+
+
+// var a=10    // variable declartion    
+    //    | assign
+
+// a=20 // reintilaztion
+
+// console.log(a)
+
+// var a=30   // redecalare
+
+// console.log(a);
+
+
+// let
+
+// let a=10
+
+// a=20
+
+// let a=30
+
+// console.log(a);
+
+
+//const
+
+// const a=20
+
+// a=30
+
+// const a=40
+
+// console.log(a)
+
+
+// non primitive data types
+
+// array
+//object
+//function
+
+// array 
+
+// collection of value
+
+// orderwise 
+
+// index base value 
+
+// index value start in 0
+
+// let arr=[]
+
+// let arr=['jayapraksam','nithish kumar',24,true,{name:"mukesh"},['Arish','alex',{age:23}]]
+
+// console.log(arr[5][2].age)
+
+//object
+
+//collection of value
+
+// key and value pair
+
+// let obj={
+    
+//     name:'Arish',
+//     age:24,
+//     collection:['Nithish','jaya prakash'],
+//     batch:{
+//         ofsd81:'arr obj',
+//         ofsd63:"crud"
+//     }
+
+// }
+
+// console.log(obj.batch.ofsd81)
+
+// operators
+
+// arithmetic operator
+
+// let a=10
+// let b=20
+
+// console.log(a+b);
+
+// console.log(a-b);
+
+// console.log(a*b);
+
+// console.log(a/b);
+
+//unary operator
+
+// postfix
+// prefix
+
+// a=15
+
+// console.log(a++)   //postfix  // post increment   // memeory  a=10 11
+                                                         //   b=20
+// console.log(a)
+
+// console.log(++a) // prefix
+
+
+// console.log(a--)  // post decreament
+
+// console.log(--a);    // pre decrement
+
+
+// type conversion
+
+// implicit conversion
+
+// explicit conversion
+
+// let a=10
+
+// a='string'
+
+// console.log(a)
+
+
+//string // ""
+
+// number //0
+
+//Boolean  // false
+
+// undefined // undefined
+
+// null 0
+  
+
+// onject // null
+
+// array // []
+
+
+
+// implicit  // type corsion
+
+
+// let a=10
+// let b='10'
+
+// let c=a+b
+
+// console.log(c)
+
+// console.log(typeof c)
+
+// console.log('10'+true);
+
+// console.log('10'+['12']);
+
+// console.log('10'-5);
+
+// console.log('10'*5)
+
+// true 1
+
+// false 0
+
+// console.log(false-1) 
+
+// console.log(null+2);
+
+// not a number
+
+// console.log(undefined+1);
+
+// console.log();
+
+// explict conversion // type casting
+
+// String
+// Number
+// Boolean
+
+// console.log(String({}))
+
+// console.log(Number([]))
+
+// console.log(Boolean([1]))
+
+// console.log(Array({}))
+
+// console.log(Object());
+
+// console.log(20>=20)
+
+// console.log(10<=10)
+
+// console.log('10'==10)  // 10==10
+
+// console.log('1'==true)
+
+// console.log(1!=10)
+
+// console.log(10===10);
+
+// console.log(10!==10);
+
+// console.log(10!=9);
+
+// console.log(10=='10'|| 0==null)
+
+// console.log(!0)
+
+// let a=10
+
+// a/=20
+
+// console.log(a);
+
+// console.log(20%7);
+
+// console.log(typeof )
+
+// control flow statement
+
+// conditional statement
+// loop statement
+
+// if 
+// if else
+// else if
+// nested else if
+
+if(10==='10'||20===20){
+    console.log('true')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
